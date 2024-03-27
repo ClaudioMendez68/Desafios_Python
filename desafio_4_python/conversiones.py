de3d6b4c36417dd51d29a1ae483d0822bdae28dc
Claudio Mendez <claudio.mendez.online@gmail.com>
2024-03-27 13:56:40 -0300

    Se agrega archivo 'recordatorios.py' y se agregan comentarios explicativos a los archivos

--- desafio_4_python/conversiones.py
+++ desafio_4_python/conversiones.py
@@ -1,10 +1,12 @@
 import sys
 
+# Se utliza estructura de lista proporcionada por argv
 sol_peruano = float(sys.argv[1])
 peso_argentino= float(sys.argv[2])
 dolar_americano = float(sys.argv[3])
 peso_chileno = float(sys.argv[4])
 
+# Se realizan las operaciones de conversión y visualización de resultados
 print(f'Los {peso_chileno} pesos equivalen a:')
 print(f'{sol_peruano * peso_chileno} Soles')
 print(f'{peso_argentino * peso_chileno} Pesos Argentinos')
--- desafio_4_python/word_count.py
+++ desafio_4_python/word_count.py
@@ -1,11 +1,18 @@
 from sys import argv
 
+# Se captura el archivo de texto a procesar nmediante argv y se asigna a la variable texto
 with open(argv[1], "r") as file:
     texto= file.read()
-
+# Se separan los caracteres contenidos en la variable texto y se asignan a la variable palabras como una lista
+# mediante el método split()
 palabras = texto.split()
+# Se convierte el contenido string del archivo 'texto' en un set para generar un conjunto de caracteres no duplicados
 caracteres = set(texto)
+# Se convierte la lista 'palabras' en un set para generar un conjunto de palabras no duplicadas y se asigna a 
+# la variable 'palabras_distintas'
 palabras_distintas = set(palabras)
 
+# Como los sets 'caracteres' y 'palabras_distintas' contienen elementos no duplicados, se obtiene la cantidad mediante
+# el tamaño de cada conjunto
 print(f'El número de caracteres distintos es: {len(caracteres)}')
 print(f'El número de palabras distintas es: {len(palabras_distintas)}')
