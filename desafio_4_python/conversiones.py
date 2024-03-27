8a890347386428b44ca3b7e4533efc81ff05ea49
Claudio Mendez <claudio.mendez.online@gmail.com>
2024-03-27 14:38:32 -0300

    Se corrige archivo conversiones.py

--- desafio_4_python/conversiones.py
+++ desafio_4_python/conversiones.py
@@ -1,13 +1,14 @@
 import sys
 
-# Se utliza estructura de lista proporcionada por argv
-sol_peruano = float(sys.argv[1])
-peso_argentino= float(sys.argv[2])
-dolar_americano = float(sys.argv[3])
-peso_chileno = float(sys.argv[4])
-
+# Se crea estructura de diccionario
+conversion = {
+    'sol_peruano' : float(sys.argv[1]),
+    'peso_argentino': float(sys.argv[2]),
+    'dolar_americano' : float(sys.argv[3]),
+    'peso_chileno' : int(sys.argv[4])
+}
 # Se realizan las operaciones de conversión y visualización de resultados
-print(f'Los {peso_chileno} pesos equivalen a:')
-print(f'{sol_peruano * peso_chileno} Soles')
-print(f'{peso_argentino * peso_chileno} Pesos Argentinos')
-print(f'{dolar_americano * peso_chileno} Dólares')
+print(f'Los {conversion['peso_chileno']} pesos equivalen a:')
+print(f'{conversion['sol_peruano'] * conversion['peso_chileno']} Soles')
+print(f'{conversion['peso_argentino'] * conversion['peso_chileno']} Pesos Argentinos')
+print(f'{conversion['dolar_americano'] * conversion['peso_chileno']} Dólares')
